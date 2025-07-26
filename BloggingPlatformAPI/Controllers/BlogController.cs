5ffb6bee1b1c699314b715de63e42cd276af10b4
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2025-07-26 01:19:42 -0400

    Initial commit

---
+++ BloggingPlatformAPI/Controllers/BlogController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BloggingPlatformAPI.Controllers;
+
+[ApiController]
+[Route("[controller]/[action]")]
+public class BlogController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return Ok("Hello World!");
+    }
+}
