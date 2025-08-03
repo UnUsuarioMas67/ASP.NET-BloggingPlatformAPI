616a4c1eb4ce95f29b56e7c2f5763d5d0eb7e260
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2025-08-02 23:16:35 -0400

    Remove RequiredAttribute from Tags property at RequestPostModel.cs

--- BloggingPlatformAPI/Models/RequestPostModel.cs
+++ BloggingPlatformAPI/Models/RequestPostModel.cs
@@ -7,5 +7,5 @@ public class RequestPostModel
     [Required, StringLength(100)] public string Title { get; set; }
     [Required, StringLength(int.MaxValue)] public string Content { get; set; }
     [Required, StringLength(50)] public string Category { get; set; }
-    [Required] public List<string> Tags { get; set; }
+    public List<string> Tags { get; set; } = new List<string>();
 }
